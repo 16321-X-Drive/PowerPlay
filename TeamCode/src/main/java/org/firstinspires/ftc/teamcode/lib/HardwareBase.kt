0b7eb7fcd311806c1d31f538a0b7b3e0f1756d02
Daniel James <danieldragonhatcher@gmail.com>
2022-10-06 20:54:45 -0700

    Tiny cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/HardwareBase.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/HardwareBase.kt
@@ -6,12 +6,10 @@ open class HardwareBase(private val opMode: LinearOpMode) {
     init {
         val fields = this::class.java.declaredFields
         fields.toList().forEach { field ->
-            val annotation = field.getAnnotation(UseDevice::class.java)
-            if (annotation != null) {
-                val name = annotation.name.ifEmpty { field.name }
-                field.set(this, opMode.hardwareMap.get(field.type, name))
-                println("field `${field.name}` as `$name` w/ type ${field.type}")
-            }
+            val annotation = field.getAnnotation(UseDevice::class.java) ?: return@forEach
+            val name = annotation.name.ifEmpty { field.name }
+            field.set(this, opMode.hardwareMap.get(field.type, name))
+            println("field `${field.name}` as `$name` w/ type ${field.type}")
         }
     }
 }
